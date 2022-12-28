0b7d36d641365b6893d06a2a12cbe9769d84fdf6
Arnold <arnoldchow@outlook.com>
2022-12-28 21:36:43 +0800

    Update logUtil

---
+++ app/src/main/java/dlans/bluetoothapp/LogUtil.java
@@ -0,0 +1,41 @@
+package dlans.bluetoothapp;
+
+public class LogUtil {
+    public static final int VERBOSE = 1;
+    public static final int DEBUG = 2;
+    public static final int INFO = 3;
+    public static final int WARN = 4;
+    public static final int ERROR = 5;
+    public static final int NOTHING = 6;
+    public static int level = VERBOSE;
+
+    public static void v(String tag, String msg) {
+        if (level <= VERBOSE) {
+            android.util.Log.v(tag, msg);
+        }
+    }
+
+    public static void d(String tag, String msg) {
+        if (level <= DEBUG) {
+            android.util.Log.d(tag, msg);
+        }
+    }
+
+    public static void i(String tag, String msg) {
+        if (level <= INFO) {
+            android.util.Log.i(tag, msg);
+        }
+    }
+
+    public static void w(String tag, String msg) {
+        if (level <= WARN) {
+            android.util.Log.w(tag, msg);
+        }
+    }
+
+    public static void e(String tag, String msg) {
+        if (level <= ERROR) {
+            android.util.Log.e(tag, msg);
+        }
+    }
+}
